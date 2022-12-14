5188513df5fdb3d0d860c609734de74ab2088c16
Radek Křikava <krikavar.03@spst.cz>
2022-12-14 18:37:13 +0100

    Přidejte soubory projektu.

---
+++ Vyjimky3/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Vyjimky3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Vypocitat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 56);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Vyjimky";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Vyjimky3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vyjimky3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int n = Int32.Parse(textBox1.Text);
+                if (n <= 0) { throw new Exception("Zadej větší číslo!"); }
+                int cisel = 0;
+                double soucet = 0;
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = openFileDialog.FileName;
+                    using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        string s = "";
+                        BinaryReader br = new BinaryReader(fs);
+                        while (cisel < n)
+                        {
+                            char ch = br.ReadChar();
+                            if ((int)ch == 46) { ch = ','; } 
+                            s += ch;
+
+                            if ((int)ch == 13){
+                                soucet += Double.Parse(s);
+                                s = "";
+                                cisel++;
+                            }
+                        }
+                    }
+                    soucet /= n;
+                    MessageBox.Show("Aritmeticky prumer prvnich cisel je " + soucet);
+                }
+            }
+            catch (OverflowException) { 
+                MessageBox.Show("Preteceni, zadej mensi cislo!"); 
+            }
+            catch (FormatException) { 
+                MessageBox.Show("Špatný formát!");
+            }
+            catch (FileNotFoundException) { 
+                MessageBox.Show("Soubor nebyl nalezen!"); 
+            }
+            catch (EndOfStreamException) { 
+                MessageBox.Show("Konec streamu, zadej mensi cislo!");
+            }
+            catch (DivideByZeroException) { 
+                MessageBox.Show("Deleni nulou!"); 
+            }
+            catch (Exception exception) { 
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
